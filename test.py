327d6a323d12b88a7df4f98fb926c03a09e2bccf
bhornby <70747776+bhornby@users.noreply.github.com>
2021-05-10 11:44:21 +0100

    working player spawn
    player can no longer be generated in a wall

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import pygame
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
+YELLOW = (255,255,0)
 #black screen
 size = (1000,720)
 
@@ -19,6 +20,44 @@ done = False
 #screen refresh rate
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite): 
+    def __init__(self,colour,width,height,speed):
+        super().__init__()
+        
+        #set player dimentions
+        self.speed_x = 0
+        self.speed_y = 0
+        self.image = pygame.Surface([width,height])
+        self.image.fill(colour)
+        
+        #set position of player need to make it so the player starts in a spot with no wall
+        self.rect = self.image.get_rect()
+        for i in map:
+            if i == 0:
+                self.rect.x,self.rect.y  = wall.rect.x,wall.rect.y
+                
+        
+        self.old_x =  self.rect.x
+        self.old_y = self.rect.y
+        
+    def update(self):
+        
+        self.rect.x = self.rect.x + self.speed_x
+        self.rect.y = self.rect.y + self.speed_y
+        
+        wall_hit_list = pygame.sprite.spritecollide(my_player, wall_group, False)
+        for x in wall_hit_list:
+            self.rect.x =  self.old_x 
+            self.rect.y =  self.old_y 
+            self.speed_x = 0
+            self.speed_y = 0
+
+        self.old_y = self.rect.y 
+        self.old_x = self.rect.x 
+            
+    def player_set_speed(self,x,y):
+        self.speed_x = x
+        self.speed_y = y
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self,colour,width,height,x,y):
@@ -33,10 +72,15 @@ all_sprite_group = pygame.sprite.Group()
 
 wall_group = pygame.sprite.Group()
 
+wall_count = 110
 numcols = 25
 numrows = 25
-map = [[random.randint(0,1) for i  in range(numcols)]for j in range(numrows)]
+# make it so limited number of walls so that there will always be a route. so there needs to be counter that loses one each time a wall is drawn
+for i in range(wall_count):
+    map = [[random.randint(0,1) for i  in range(numcols)]for j in range(numrows)]
+# next i
 print(map)
+
 
 x = 0
 y = 0
@@ -55,7 +99,8 @@ for row in map:
     y = y + 40
 #next row
 
-
+my_player = Player(YELLOW,20,20,5)
+all_sprite_group.add(my_player)
 
 pygame.init()
 #game loop
@@ -64,6 +109,21 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+         
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:#if left key is pressed
+                my_player.player_set_speed(-1,0)
+            elif event.key == pygame.K_RIGHT:
+                my_player.player_set_speed(1,0)
+            elif event.key == pygame.K_UP:
+                my_player.player_set_speed(0,-1)
+            elif event.key == pygame.K_DOWN:
+                my_player.player_set_speed(0,1)
+                
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                my_player.player_set_speed(0,0)
+    
     #update all sprites    
     all_sprite_group.update()
     #screen background is black

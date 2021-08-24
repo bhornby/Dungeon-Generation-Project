2f2d55a6c3333aa773b6eab5017df3ee72450904
bhornby <70747776+bhornby@users.noreply.github.com>
2021-08-24 17:11:49 +0100

    code for v or h split in leave
    currently set with dungoen full of # so currently no rooms are being cleared out. but to start clearing out the rooms we need to determine where to split  so by determining a segment height and width using the new value each time the function is called we get different splits vertical or horizonal
    and this is determined by the values of said data relative to the self.MAx values

--- Test file for bsp alg.py
+++ Test file for bsp alg.py
@@ -19,6 +19,43 @@ class Room:
         self.col = c
         self.height = h
         self.width = w
+        
+class DungeonGenerator:
+    def __init__(self, w, h):
+        self.MAX = 1000 # Cutoff for when we want to stop splitting the tree
+        self.width = w
+        self.height = h
+        self.leaves = [] #creating a list for each so we can split and add
+        self.dungeon = []
+        self.rooms = []
+
+        for h in range(self.height):
+            row = []
+            for w in range(self.width):
+                row.append(DungeonSqr('#'))
+                
+                #filling out the dungeon with #'s
+
+            self.dungeon.append(row)
+
+
+    def random_split(self, min_row, min_col, max_row, max_col):
+        # We want to keep splitting until the sections get down to the threshold set in self.MAX
+        seg_height = max_row - min_row 
+        seg_width = max_col - min_col
+
+        if seg_height < self.MAX and seg_width < self.MAX:
+            self.leaves.append((min_row, min_col, max_row, max_col)) #adding a new leaf
+            
+        elif seg_height < self.MAX and seg_width >= self.MAX:
+            self.split_on_vertical(min_row, min_col, max_row, max_col)
+            
+        elif seg_height >= self.MAX and seg_width < self.MAX:
+            self.split_on_horizontal(min_row, min_col, max_row, max_col)
+            
+            #depending on the value of max-row - min_row you can either get a vertical or horizontal row almost a 50/50 chance
+       
+
 
 
 

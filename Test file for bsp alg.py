8237ab88c278f686fead75ade0bed7a719080ee9
bhornby <70747776+bhornby@users.noreply.github.com>
2021-08-24 18:12:34 +0100

    Roooms are now carved
    this was so much work - so the main problem initially was that the rooms were being carved either too close together or too far apart or in the worst case all the rooms were overlapping. to solve this in the horizontal and vertical split function i incremented the min col and nim row  - conditionally to account for the lack in each one when compared to the self.MAX. when actually carving the rooms  we dont want every room to be filled resulting in a uniform dungon. therefore if the room was determined to be above 80 percent filled then we would reduce the width and height of said leaf accordingly. now the section assigned to the leaf is defined its time to make the room as we dont want tiny rooms making the game dificult to play i set it to within 60 percent fo the available section

--- Test file for bsp alg.py
+++ Test file for bsp alg.py
@@ -14,29 +14,41 @@ class DungeonSqr:
         return self.sqr
 
 class Room:
-    def __init__(self, r, c, h, w):
-        self.row = r
-        self.col = c
-        self.height = h
-        self.width = w
+    def __init__(self, row, col, height, width):
+        self.row = row
+        self.col = col
+        self.height = height
+        self.width = width
         
 class DungeonGenerator:
-    def __init__(self, w, h):
-        self.MAX = 1000 # Cutoff for when we want to stop splitting the tree
-        self.width = w
-        self.height = h
+    def __init__(self, width, height):
+        self.MAX = 10 # Cutoff for when we want to stop splitting the tree
+        self.width = width
+        self.height = height
         self.leaves = [] #creating a list for each so we can split and add
         self.dungeon = []
         self.rooms = []
 
-        for h in range(self.height):
+        for height in range(self.height):
             row = []
-            for w in range(self.width):
+            for width in range(self.width):
                 row.append(DungeonSqr('#'))
                 
                 #filling out the dungeon with #'s
 
             self.dungeon.append(row)
+    
+    def split_on_horizontal(self, min_row, min_col, max_row, max_col):
+        #using the choice module from random allows ease of code writing no need for large array and item selection code
+        split = (min_row + max_row) // 2 + choice((-2, -1, 0, 1, 2))
+        self.random_split(min_row, min_col, split, max_col)
+        self.random_split(split + 1, min_col, max_row, max_col)
+
+    def split_on_vertical(self, min_row, min_col, max_row, max_col):        
+        split = (min_col + max_col) // 2 + choice((-2, -1, 0, 1, 2))
+        self.random_split(min_row, min_col, max_row, split)
+        self.random_split(min_row, split + 1, max_row, max_col)
+        #the plus ones allow for extra spacing in between rooms otherwise there will either be overlaps between rooms or the rooms will be too close together
 
 
     def random_split(self, min_row, min_col, max_row, max_col):
@@ -54,8 +66,52 @@ class DungeonGenerator:
             self.split_on_horizontal(min_row, min_col, max_row, max_col)
             
             #depending on the value of max-row - min_row you can either get a vertical or horizontal row almost a 50/50 chance
-       
+        else:
+            if random() < 0.5:
+                self.split_on_horizontal(min_row, min_col, max_row, max_col)
+                
+            else:
+                self.split_on_vertical(min_row, min_col, max_row, max_col)
+        
+        #at the end of the random split you are left with the self.MAX number of leaves in the self.leaves list
+          
+    def carve_rooms(self):
+        for leaf in self.leaves:
+            # We don't want to fill in every possible room or the  dungeon looks too uniform
+            if random() > 0.80: continue
+            section_width = leaf[3] - leaf[1]
+            section_height = leaf[2] - leaf[0]
 
+            # The actual room's height and width will be 60-100% of the 
+            # available section. 
+            room_width = round(randrange(60, 100) / 100 * section_width)
+            room_height = round(randrange(60, 100) / 100 * section_height)
+
+            room_start_row = leaf[0]
+            room_start_col = leaf[1]
+    
+            self.rooms.append(Room(room_start_row, room_start_col, room_height, room_width))
+            for row in range(room_start_row, room_start_row + room_height):
+                for col in range(room_start_col, room_start_col + room_width):
+                    self.dungeon[row][col] = DungeonSqr('|')
+
+    def generate_map(self):
+        self.random_split(1, 1, self.height - 1, self.width - 1)
+        self.carve_rooms()
+
+    def print_map(self):
+        for r in range(self.height):
+            row = ''
+            for c in range(self.width):
+                row += self.dungeon[r][c].get_ch()
+            print(row)
+
+dungeon = DungeonGenerator(75, 40)
+dungeon.generate_map()
+dungeon.print_map()
+
+    
+    
 
 
 
